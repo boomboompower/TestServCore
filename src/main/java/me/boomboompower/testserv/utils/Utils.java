a4e0d7c9926b6efa9afd02b1c32a353e6b91a4e0
boomboompower <boomboompowermc@gmail.com>
2016-06-04 15:01:23 +1000

    Update Utils.java

--- src/main/java/me/boomboompower/testserv/utils/Utils.java
+++ src/main/java/me/boomboompower/testserv/utils/Utils.java
@@ -5,23 +5,49 @@ package me.boomboompower.testserv.utils;
 * by boomboompower 25/04/2016
 */
 
-import net.md_5.bungee.api.ChatColor;
-
-import org.bukkit.Bukkit;
+import org.bukkit.*;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class Utils {
 
-    public static boolean permissionCheck(Player player, String permission) {
-        permission = permission.toLowerCase().replace(" ", ".");
+    private Utils() {}
 
-        if (player.hasPermission(permission)) {
-            return true;
-        } else {
-            return false;
+    public static boolean permissionCheck(Player player, String permission) {
+        return player.hasPermission(permission.toLowerCase());
+    }
+
+    public static int lowestLocation() {
+        return -35;
+    }
+
+    public static int highestLocation() {
+        return 300;
+    }
+
+    public static Location spawnLocation(World world) {
+        return new Location(world, -167.0D, 123.0D, 334.0D);
+    }
+
+    public static String getArguments(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 1; i < args.length; i++) {
+            builder.append(args[i]);
+            builder.append(" ");
         }
+        return builder.toString().trim();
+    }
+
+    public static String alertFormat() {
+        return translate("&4&lALERT: &c&l");
+    }
+
+    public static String denyMessage() {
+        return translate("&cI\'m sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
     }
 
     public static String translate(String message) {
@@ -30,26 +56,34 @@ public class Utils {
         return message;
     }
 
-    public static void sendToPlayer(Player player, String message) {
-        player.sendMessage(translate(message));
-    }
+    public static String stripColor(String message) {
+        message = ChatColor.stripColor(message);
 
-    public static void duelSend(CommandSender sender, String message) {
-        if (sender instanceof ConsoleCommandSender) {
-            sendToConsole(message);
-        } else if (sender instanceof Player) {
-            sendToPlayer((Player) sender, message);
-        } else {
-            sendToConsole("&cAn unexpected error occured while using the &4duelSend(CommandSender, String) &cmethod!");
-        }
-    }
-
-    public static String denyMessage() {
-        return translate("&cI\'m sorry, but you do not have permission to perform this command. Please contact the server administrators if you believe that this is in error.");
+        return message;
     }
 
     public static String kickMessage() {
         return translate("&4&lYou were warned.");
+    }
+
+    public static String denyMessage(String reason, String warning) {
+        return translate("&cYou do not have permission to " + reason + "! &4(" + warning + ")&c.");
+    }
+
+    public static void openInventory(Player player, Inventory inventory) {
+        player.openInventory(inventory);
+    }
+
+    public static void closeInventory(Player player) {
+        player.closeInventory();
+    }
+
+    public static void sendToWorld(Player player, World world) {
+        player.teleport(spawnLocation(world), PlayerTeleportEvent.TeleportCause.SPECTATE);
+    }
+
+    public static void sendSound(Player player, Sound sound, float pitch) {
+        player.playSound(player.getLocation(), sound, (float) 10, pitch);
     }
 
     public static void kickPlayer(Player p) {
@@ -60,16 +94,51 @@ public class Utils {
         p.kickPlayer(translate(message));
     }
 
-    public static String denyMessage(String reason, String warning) {
-        String message = translate("&cYou do not have permission to " + reason + "! &4(" + warning + ")&c.");
-        return message;
-    }
-
     public static void sendDenyMessage(Player player) {
         player.sendMessage(denyMessage());
     }
 
+    public static void sendToPlayer(Player player, String message) {
+        player.sendMessage(translate(message));
+    }
+
     public static void sendToConsole(String message) {
         Bukkit.getConsoleSender().sendMessage(translate(message));
+    }
+
+    public static void duelSend(CommandSender sender, String message) {
+        if (sender instanceof ConsoleCommandSender) {
+            sendToConsole(message);
+        } else if (sender instanceof Player) {
+            sendToPlayer((Player) sender, message);
+        } else {
+            sendToConsole("&cAn unexpected error occured while using the &4duelSend(CommandSender, String)&c method!");
+        }
+    }
+
+    public static void sendActionBar(String message, Player... player) {
+        for (Player players : player) {
+            ReflectionUtils.sendActionBar(players, message);
+        }
+    }
+
+    public static void sendHeaderAndFooter(String header, String footer, Player... player) {
+        for (Player players : player) {
+            ReflectionUtils.sendHeaderAndFooter(players, header, footer);
+        }
+    }
+
+    public static void sendChatMessage(String message, Player... player) {
+        for (Player players : player) {
+            ReflectionUtils.sendChatMessage(players, message);
+        }
+    }
+
+    public static ItemStack getItemInMainHand(Player player) {
+        return player.getInventory().getItemInMainHand();
+    }
+
+    public static ItemStack getItemInOffHand(Player player) {
+        return player.getInventory().getItemInOffHand();
     }
 }
