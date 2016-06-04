198f1213c3b9d44dd7c833e9de83e32fd1dbc80a
boomboompower <boomboompowermc@gmail.com>
2016-06-04 14:49:07 +1000

    Create TestServCoreFire.java

---
+++ src/main/java/me/boomboompower/testserv/listeners/TestServCoreFire.java
@@ -0,0 +1,44 @@
+package me.boomboompower.testserv.listeners;
+
+/*
+* Made for TestServCore
+* by boomboompower 06/05/2016
+*/
+
+import me.boomboompower.testserv.TestServCore;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBurnEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+
+import static me.boomboompower.testserv.utils.Register.registerEvents;
+import static me.boomboompower.testserv.utils.Utils.*;
+
+public class TestServCoreFire implements Listener {
+
+    private TestServCore testServCore;
+
+    public TestServCoreFire(TestServCore testServCore) {
+        this.testServCore = testServCore;
+
+        registerEvents(this);
+    }
+
+    @EventHandler
+    private void onBlockBurn(BlockBurnEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    private void onBlockIgnite(BlockIgniteEvent event) {
+        if (event.getIgnitingEntity() instanceof Player) {
+            Player player = (Player) event.getIgnitingEntity();
+            if (!permissionCheck(player, "TestServ.ignite")) {
+                event.setCancelled(true);
+                sendToPlayer(player, "&cYou do not have permission to ignite this block!");
+            }
+        }
+    }
+}
