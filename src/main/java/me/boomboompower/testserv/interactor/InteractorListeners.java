0cb1fda4dc0d892c1d225a02f1ed79076e7974b4
boomboompower <boomboompowermc@gmail.com>
2016-06-04 14:43:00 +1000

    Update InteractorListeners.java

--- src/main/java/me/boomboompower/testserv/interactor/InteractorListeners.java
+++ src/main/java/me/boomboompower/testserv/interactor/InteractorListeners.java
@@ -3,8 +3,9 @@ package me.boomboompower.testserv.interactor;
 /**
  * This code is part of *Interactor*
  * A separate plugin by boomboompower
- *
+ * <p>
  * http://github.com/boomboompower/Interactor/
+ * The link above is no longer valid
  */
 
 import me.boomboompower.testserv.TestServCore;
@@ -17,5 +18,6 @@ public class InteractorListeners {
         this.testServCore = testServCore;
 
         new InteractorInteract(testServCore);
+        new InteractorDoors(testServCore);
     }
 }
