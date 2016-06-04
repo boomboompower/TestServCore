9933f5ce7b8d55deed7789e2bb24092a6052a7e6
boomboompower <boomboompowermc@gmail.com>
2016-06-04 14:33:36 +1000

    Update TestServCoreCommands.java

--- src/main/java/me/boomboompower/testserv/commands/TestServCoreCommands.java
+++ src/main/java/me/boomboompower/testserv/commands/TestServCoreCommands.java
@@ -12,9 +12,15 @@ public class TestServCoreCommands {
     private TestServCore testServCore;
 
     public TestServCoreCommands(TestServCore testServCore) {
+        this.testServCore = testServCore;
 
         new TestServCoreRemove(testServCore);
+        new TestServCoreSpawn(testServCore);
+        new TestServCoreAlert(testServCore);
+        new TestServCoreSudo(testServCore);
+        new TestServCoreKick(testServCore);
         new TestServCoreHelp(testServCore);
         new TestServCoreBan(testServCore);
+        new TestServCoreOp(testServCore);
     }
 }
