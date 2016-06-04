b006cd670a5b3cb4ed1ed61a795aaf96400d5195
boomboompower <boomboompowermc@gmail.com>
2016-06-04 14:50:20 +1000

    Update TestServCoreJoin.java

--- src/main/java/me/boomboompower/testserv/listeners/TestServCoreJoin.java
+++ src/main/java/me/boomboompower/testserv/listeners/TestServCoreJoin.java
@@ -8,57 +8,61 @@ package me.boomboompower.testserv.listeners;
 import me.boomboompower.testserv.TestServCore;
 
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import static me.boomboompower.testserv.utils.Utils.translate;
+import static me.boomboompower.testserv.listeners.TestServCoreSigns.worldItem;
+import static me.boomboompower.testserv.utils.Register.registerEvents;
+import static me.boomboompower.testserv.utils.Utils.*;
 
 public class TestServCoreJoin implements Listener {
 
     private TestServCore testServCore;
 
-    private World world = Bukkit.getWorlds().get(0);
-    private Double X = -167.0D;
-    private Double Y = 123.0D;
-    private Double Z = 334.0D;
-
     public TestServCoreJoin(TestServCore testServCore) {
         this.testServCore = testServCore;
 
-        Bukkit.getPluginManager().registerEvents(this, testServCore);
+        registerEvents(this);
     }
 
     @EventHandler
     private void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
+        double lastPlayed = p.getLastPlayed() / 0.001;
+
+        p.setMaxHealth(40D);
+        p.setHealth(40D);
+        p.setFoodLevel(20);
+
+        p.getInventory().clear();
+        p.getInventory().setItem(4, worldItem());
+
         e.setJoinMessage(translate("&7[&a+&7] &f") + p.getName());
 
         p.setCollidable(false);
 
-        p.removePotionEffect(PotionEffectType.HEALTH_BOOST);
-        p.removePotionEffect(PotionEffectType.GLOWING);
-        p.removePotionEffect(PotionEffectType.SPEED);
-        p.removePotionEffect(PotionEffectType.HEAL);
+        p.getActivePotionEffects().clear();
 
-        p.addPotionEffect(PotionEffectType.HEALTH_BOOST.createEffect(Integer.MAX_VALUE, 4));
         p.addPotionEffect(PotionEffectType.SPEED.createEffect(Integer.MAX_VALUE, 3));
-        p.addPotionEffect(PotionEffectType.HEAL.createEffect(1, 100));
 
+        // Wait 5 ticks so this runs after any other plugin.
         sendToSpawn(p, 5);
+
+        sendHeaderAndFooter("&cWelcome to &4&lTESTSERV", "&bOwner &f: &9boomboompower", p);
+        sendActionBar("&4&lWelcome to the server! &c&l" + p.getName() + "!", p);
+        sendChatMessage("&4&lWelcome to the server! &c&l" + p.getName() + "!", p);
+        sendChatMessage("&aYou last played: &3" + lastPlayed + "!");
     }
 
     private void sendToSpawn(final Player player, int time) {
         new BukkitRunnable() {
             @Override
             public void run() {
-                player.teleport(new Location(world, X, Y, Z), PlayerTeleportEvent.TeleportCause.COMMAND);
+                sendToWorld(player,Bukkit.getWorlds().get(0));
             }
         }.runTaskLater(testServCore, time);
     }
