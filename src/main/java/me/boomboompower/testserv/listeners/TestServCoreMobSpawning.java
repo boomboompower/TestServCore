c4258094a6224928db6425ef258737b3df521e3e
boomboompower <boomboompowermc@gmail.com>
2016-06-04 14:54:31 +1000

    Update TestServCoreMobSpawning.java

--- src/main/java/me/boomboompower/testserv/listeners/TestServCoreMobSpawning.java
+++ src/main/java/me/boomboompower/testserv/listeners/TestServCoreMobSpawning.java
@@ -6,16 +6,21 @@ package me.boomboompower.testserv.listeners;
 */
 
 import me.boomboompower.testserv.TestServCore;
-
-import org.bukkit.Bukkit;
+import org.bukkit.EntityEffect;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.material.MaterialData;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.List;
+import java.util.Random;
+
+import static me.boomboompower.testserv.utils.Register.registerEvents;
 import static me.boomboompower.testserv.utils.Utils.*;
 
 public class TestServCoreMobSpawning implements Listener {
@@ -25,7 +30,7 @@ public class TestServCoreMobSpawning implements Listener {
     public TestServCoreMobSpawning(TestServCore testServCore) {
         this.testServCore = testServCore;
 
-        Bukkit.getPluginManager().registerEvents(this, testServCore);
+        registerEvents(this);
     }
 
     @EventHandler(priority = EventPriority.HIGH)
@@ -38,10 +43,20 @@ public class TestServCoreMobSpawning implements Listener {
 
         } else if (entity instanceof Zombie) {
             Zombie zombie = (Zombie) entity;
-            zombie.setBaby(false);
             zombie.setVillager(true);
-            zombie.setVillagerProfession(Villager.Profession.FARMER);
-            zombie.addPotionEffect(PotionEffectType.FIRE_RESISTANCE.createEffect(Integer.MAX_VALUE, 1));
+            if (zombie instanceof PigZombie) {
+                PigZombie pigZombie = (PigZombie) entity;
+                pigZombie.addPotionEffect(PotionEffectType.HEALTH_BOOST.createEffect(Integer.MAX_VALUE, 10));
+                pigZombie.setAngry(true);
+                pigZombie.setAnger(100000);
+            } else {
+                if (zombie.isBaby()) {
+                    zombie.addPotionEffect(PotionEffectType.SPEED.createEffect(Integer.MAX_VALUE, 3));
+                } else {
+                    zombie.setVillagerProfession(Villager.Profession.FARMER);
+                    zombie.addPotionEffect(PotionEffectType.FIRE_RESISTANCE.createEffect(Integer.MAX_VALUE, 1));
+                }
+            }
 
         } else if (entity instanceof Skeleton) {
             Skeleton skeleton = (Skeleton) entity;
@@ -54,32 +69,46 @@ public class TestServCoreMobSpawning implements Listener {
             Slime slime = (Slime) entity;
             slime.setSize(5);
 
-        } else if (entity instanceof MagmaCube) {
-            MagmaCube magmaCube = (MagmaCube) entity;
-            magmaCube.setSize(15);
-
         } else if (entity instanceof Guardian) {
             Guardian guardian = (Guardian) entity;
             guardian.setElder(true);
-
-        } else if (entity instanceof PigZombie) {
-            PigZombie pigZombie = (PigZombie) entity;
-            pigZombie.addPotionEffect(PotionEffectType.HEALTH_BOOST.createEffect(Integer.MAX_VALUE, 10));
-            pigZombie.setAngry(true);
-            pigZombie.setAnger(100);
 
         } else if (entity instanceof Enderman) {
             Enderman enderman = (Enderman) entity;
 
             enderman.addPotionEffect(PotionEffectType.FIRE_RESISTANCE.createEffect(Integer.MAX_VALUE, 5));
-            enderman.setCarriedMaterial(Material.DIAMOND_BLOCK.getNewData((byte) 1));
+
+            Integer integer = new Random().nextInt(5);
+
+            MaterialData mat;
+
+            switch (integer) {
+                case 0:
+                    mat = Material.EMERALD_BLOCK.getNewData((byte) 0);
+                    break;
+                case 1:
+                    mat = Material.DIAMOND_BLOCK.getNewData((byte) 0);
+                    break;
+                case 2:
+                    mat = Material.GOLD_BLOCK.getNewData((byte) 0);
+                    break;
+                case 3:
+                    mat = Material.IRON_BLOCK.getNewData((byte) 0);
+                    break;
+                default:
+                    mat = Material.COAL_BLOCK.getNewData((byte) 0);
+                    break;
+            }
+            enderman.setCarriedMaterial(mat);
 
         } else if (entity instanceof EnderDragon) {
             EnderDragon enderDragon = (EnderDragon) entity;
             enderDragon.setCustomName(translate("&5&lThe EnderDragon!"));
 
-        } else if (entity instanceof Shulker) { // TODO add something *UNIQUE*
+        } else if (entity instanceof Shulker) {
             Shulker shulker = (Shulker) entity;
+            shulker.setMaxHealth(50D);
+            shulker.setHealth(50D);
 
         } else if (entity instanceof Snowman) {
             Snowman snowman = (Snowman) entity;
@@ -89,23 +118,50 @@ public class TestServCoreMobSpawning implements Listener {
             IronGolem ironGolem = (IronGolem) entity;
             ironGolem.setPlayerCreated(true);
 
-        } else if (entity instanceof Spider) { // TODO add something *UNIQUE*
+        } else if (entity instanceof Spider) {
             Spider spider = (Spider) entity;
+            if (spider instanceof CaveSpider) {
+                CaveSpider caveSpider = (CaveSpider) entity;
+                caveSpider.addPotionEffect(PotionEffectType.SLOW.createEffect(Integer.MAX_VALUE, 3));
+            } else {
+                List<Entity> nearby = spider.getNearbyEntities(4D, 4D, 4D);
+                if (nearby instanceof Player) {
+                    Player player = (Player) nearby;
+                    spider.setTarget(player);
+                }
+            }
 
-        } else if (entity instanceof CaveSpider) { // TODO add something *UNIQUE*
-            CaveSpider caveSpider = (CaveSpider) entity;
-
-        } else if (entity instanceof Wither) { // TODO add something *UNIQUE*
+        } else if (entity instanceof Wither) {
             Wither wither = (Wither) entity;
+            wither.playEffect(EntityEffect.DEATH);
+            wither.addPotionEffect(PotionEffectType.WEAKNESS.createEffect(Integer.MAX_VALUE, 3));
+
+        } else if (entity instanceof Blaze) {
+            Blaze blaze = (Blaze) entity;
+            blaze.setMaxHealth(50D);
+            blaze.setHealth(50D);
+
+        } else if (entity instanceof Giant) {
+            Giant giant = (Giant) entity;
+            giant.addPotionEffect(PotionEffectType.SLOW.createEffect(Integer.MAX_VALUE, 5));
+
+        } else if (entity instanceof Endermite) {
+            Endermite endermite = (Endermite) entity;
+            endermite.addPotionEffect(PotionEffectType.ABSORPTION.createEffect(Integer.MAX_VALUE, 2));
+            endermite.setMaxHealth(30D);
+            endermite.setHealth(30D);
+
+        } else if (entity instanceof Player) {
+            Player p = (Player) entity;
+            p.setDisplayName(translate("&5&l") + p.getName());
+            sendSound(p, Sound.UI_BUTTON_CLICK, 0);
+
+            p.addPotionEffect(PotionEffectType.HEALTH_BOOST.createEffect(Integer.MAX_VALUE, 4));
+            p.addPotionEffect(PotionEffectType.SPEED.createEffect(Integer.MAX_VALUE, 3));
+            p.addPotionEffect(PotionEffectType.HEAL.createEffect(1, 100));
 
         } else {
-            entity.setCustomName(translate("&aEntity not supported"));
-
-        } if (entity instanceof LivingEntity) {
-            LivingEntity living = (LivingEntity) entity;
-            living.setCollidable(false);
-            living.setAI(true);
-            living.setMaximumAir(10);
+            entity.setCustomName(translate("&cEntity &4not&c supported"));
         }
     }
 }
